1d52eca6cecbb21071346502e33e2084ab638a7c
farhanmazario <farhan.mazario@code.id>
2026-04-09 10:25:38 +0300

    chore: initialize Spring Boot project

---
+++ src/test/java/com/voluntrack/volunteerplatform/VolunteerplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voluntrack.volunteerplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
