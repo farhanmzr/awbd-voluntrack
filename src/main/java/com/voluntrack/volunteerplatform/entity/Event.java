e3a2e38664addf4f1a532f9be8b470d939578d3b
farhanmazario <farhan.mazario@code.id>
2026-04-23 00:06:52 +0300

    feat: admin event CRUD

--- src/main/java/com/voluntrack/volunteerplatform/entity/Event.java
+++ src/main/java/com/voluntrack/volunteerplatform/entity/Event.java
@@ -1,14 +1,30 @@
 package com.voluntrack.volunteerplatform.entity;
 
-import com.voluntrack.volunteerplatform.enums.EventStatus;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.voluntrack.volunteerplatform.enums.EventStatus;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "events")
@@ -27,9 +43,11 @@ public class Event {
     @Column(nullable = false, columnDefinition = "TEXT")
     private String description;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     @Column(name = "start_date_time", nullable = false)
     private LocalDateTime startDateTime;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     @Column(name = "end_date_time", nullable = false)
     private LocalDateTime endDateTime;
 
